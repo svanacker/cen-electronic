63ebe37b0e38c9df3478967ab418449223cdb1a4
f4deb <f4deb1@gmail.com>
2014-11-27 22:21:44 +0100

    MAJ mineur

--- drivers/robotConfig/robotConfigPic32.c
+++ drivers/robotConfig/robotConfigPic32.c
@@ -13,10 +13,26 @@
  */
 
 signed int _robotConfigReadInt (RobotConfig* robotConfig){
-    // Init the portb input selected as I/O
-    AD1PCFG = 0b1011111011000000;
-
     int value = 0;
+
+
+
+    value = ((PORTG & 0x0004) << 11) | // PORTG2
+            ((PORTC & 0x2000) >> 1) | // PORTC13
+            ((PORTB & 0x8000) >> 4)|// PORTB15
+            ((PORTB & 0x3E00) >> 3) | // PORTB9 -> PORTB13
+            ((PORTB & 0x00C0) >> 2) | // PORTB6 - PORTB7
+            ((PORTD & 0x01E0) >> 5); // PORTD5 ->PORTD8
+
+    return (value);
+}
+
+void initRobotConfigPic32(RobotConfig* robotConfig) {
+    initRobotConfig(robotConfig, _robotConfigReadInt);
+        // Init the portb input selected as I/O
+    AD1PCFG = 0xBEC0;//0b1011111011000000;
+
+    //Init the port selected as Input
     TRISDbits.TRISD5 = 1;
     TRISDbits.TRISD6 = 1;
     TRISDbits.TRISD7 = 1;
@@ -31,17 +47,4 @@ signed int _robotConfigReadInt (RobotConfig* robotConfig){
     TRISBbits.TRISB15 = 1;
     TRISCbits.TRISC13 = 1;
     TRISGbits.TRISG2 = 1;
-
-    value = ((PORTG & 0x0004) << 11) | // PORTG2
-            ((PORTC & 0x2000) >> 1) | // PORTC13
-            ((PORTB & 0x8000) >> 4)|// PORTB15
-            ((PORTB & 0x3E00) >> 3) | // PORTB9 -> PORTB13
-            ((PORTB & 0x00C0) >> 2) | // PORTB6 - PORTB7
-            ((PORTD & 0x01E0) >> 5); // PORTD5 ->PORTD8
-
-    return (value);
-}
-
-void initRobotConfigPic32(RobotConfig* robotConfig) {
-    initRobotConfig(robotConfig, _robotConfigReadInt);
 }
