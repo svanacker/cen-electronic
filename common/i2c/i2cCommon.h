f434981ccc2fd79eb0181f5ec4376a6d18defdd5
Stephane Vanacker <svanacker@talentsoft.com>
2015-02-15 18:10:59 +0100

    I2C : be able to manage I2C_1 and I2C_4 #61

--- common/i2c/i2cCommon.h
+++ common/i2c/i2cCommon.h
@@ -1,6 +1,30 @@
 #ifndef I2C_COMMON_H
 #define I2C_COMMON_H
 
+// forward declaration
+struct I2c;
+typedef struct I2c I2c;
+
+/** Define the index of i2c port 1. */
+#define I2C_PORT_1         1
+
+/** Define the index of i2c port 2. */
+#define I2C_PORT_2         2
+
+/** Define the index of i2c port 3. */
+#define I2C_PORT_3         3
+
+/** Define the index of i2c port 4. */
+#define I2C_PORT_4         4
+
+/**
+ * Defines the contract for an I2C.
+ */
+struct I2c {
+    /** port Index. */
+    unsigned portIndex;
+};
+
 #define NACK    0
 
 /**
@@ -11,6 +35,7 @@ void WaitI2C();
 /**
  * Indirection for I2C used to manage 30F/32 compatibility.
  */
+// void portableStartI2C(I2c* i2c);
 void portableStartI2C();
 
 /**
