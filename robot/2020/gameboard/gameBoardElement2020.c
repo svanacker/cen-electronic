41130aa957bdd2010d32f16ca5e1fd76d2ae49b9
Stephane Vanacker <svanacker@gmail.com>
2020-03-29 23:03:53 +0200

    Add 2020 Gameboard elements

--- robot/2020/gameboard/gameBoardElement2020.c
+++ robot/2020/gameboard/gameBoardElement2020.c
@@ -41,10 +41,105 @@ bool windowExclusionNotReachable2020(GameBoard* gameBoard, int* element, float x
     return true;
 }
 
+void cupRedAndGreen(GameBoard* gameBoard, float x, float y) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawCircle(gameBoard, x, y, CUP_RADIUS, '*');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawCircle(gameBoard, x, GAMEBOARD_HEIGHT - y, CUP_RADIUS, '*');
+}
+
+void cupGreenAndRed(GameBoard* gameBoard, float x, float y) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawCircle(gameBoard, x, y, CUP_RADIUS, '*');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawCircle(gameBoard, x, GAMEBOARD_HEIGHT - y, CUP_RADIUS, '*');
+}
+
+void startAreaPrint(GameBoard* gameBoard, int* element) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_BLUE);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_GREEN_LEFT_X, START_AREA_RECTANGLE_GREEN_LEFT_Y, START_AREA_RECTANGLE_WITH, START_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_YELLOW);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_GREEN_LEFT_X, START_AREA_RECTANGLE_GREEN_LEFT_Y + 2600, START_AREA_RECTANGLE_WITH, START_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_GREEN_LEFT_X, START_AREA_RECTANGLE_GREEN_LEFT_Y, START_AREA_RECTANGLE_GREEN_WIDTH, START_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_RED_LEFT_X, START_AREA_RECTANGLE_RED_LEFT_Y, START_AREA_RECTANGLE_RED_WIDTH, START_AREA_RECTANGLE_RED_HEIGHT, '|', '-');
+
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_GREEN_LEFT_X, START_AREA_RECTANGLE_GREEN_LEFT_Y + 2600, START_AREA_RECTANGLE_GREEN_WIDTH, START_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawRectangle(gameBoard, START_AREA_RECTANGLE_RED_LEFT_X, START_AREA_RECTANGLE_RED_LEFT_Y + 2600, START_AREA_RECTANGLE_RED_WIDTH, START_AREA_RECTANGLE_RED_HEIGHT, '|', '-');
+}
+
+void protectionDropAreasPrint(GameBoard* gameBoard, int* element) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_WHITE);
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_PROTECTION_LEFT_X, DROP_AREA_RECTANGLE_PROTECTION_LEFT_Y, DROP_AREA_RECTANGLE_PROTECTION_LEFT_WIDTH, DROP_AREA_RECTANGLE_PROTECTION_LEFT_HEIGHT, '|', '-');
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_PROTECTION_LEFT_X, GAMEBOARD_HEIGHT - DROP_AREA_RECTANGLE_PROTECTION_LEFT_Y, DROP_AREA_RECTANGLE_PROTECTION_LEFT_WIDTH, DROP_AREA_RECTANGLE_PROTECTION_LEFT_HEIGHT, '|', '-');
+}
+
+void dropAreasPrint(GameBoard* gameBoard, int* element) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_GREEN_LEFT_X, DROP_AREA_RECTANGLE_GREEN_LEFT_Y, DROP_AREA_RECTANGLE_GREEN_WIDTH, DROP_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_RED_LEFT_X, DROP_AREA_RECTANGLE_RED_LEFT_Y, DROP_AREA_RECTANGLE_RED_WIDTH, DROP_AREA_RECTANGLE_RED_HEIGHT, '|', '-');
+
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_GREEN);
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_GREEN_LEFT_X, DROP_AREA_RECTANGLE_GREEN_LEFT_Y - 600, DROP_AREA_RECTANGLE_GREEN_WIDTH, DROP_AREA_RECTANGLE_GREEN_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_RED);
+    drawRectangle(gameBoard, DROP_AREA_RECTANGLE_RED_LEFT_X, DROP_AREA_RECTANGLE_RED_LEFT_Y - 600, DROP_AREA_RECTANGLE_RED_WIDTH, DROP_AREA_RECTANGLE_RED_HEIGHT, '|', '-');
+}
+
+void rightReefPrint(GameBoard* gameBoard, int* element) {
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_BLUE);
+    drawRectangle(gameBoard, RIGHT_REEF_RECTANGLE_LEFT_X, RIGHT_REEF_RECTANGLE_LEFT_Y, RIGHT_REEF_RECTANGLE_WIDTH, RIGHT_REEF_RECTANGLE_HEIGHT, '|', '-');
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_YELLOW);
+    drawRectangle(gameBoard, RIGHT_REEF_RECTANGLE_LEFT_X, RIGHT_REEF_RECTANGLE_LEFT_Y + 3000, RIGHT_REEF_RECTANGLE_WIDTH, RIGHT_REEF_RECTANGLE_HEIGHT, '|', '-');
+}
+
+void cupPrints(GameBoard* gameBoard, int* element) {
+    // Start Area Cups
+    cupRedAndGreen(gameBoard, START_AREA_CUP_RED_LEFT_X, START_AREA_CUP_RED_LEFT_Y);
+    cupGreenAndRed(gameBoard, START_AREA_CUP_GREEN_LEFT_X, START_AREA_CUP_GREEN_LEFT_Y);
+    cupRedAndGreen(gameBoard, START_AREA_CUP_RED_RIGHT_X, START_AREA_CUP_RED_RIGHT_Y);
+    cupGreenAndRed(gameBoard, START_AREA_CUP_GREEN_RIGHT_X, START_AREA_CUP_GREEN_RIGHT_Y);
+
+    // Reef Cups
+    cupRedAndGreen(gameBoard, LEFT_REEF_CUP_RED_LEFT_X, LEFT_REEF_CUP_RED_LEFT_Y);
+    cupGreenAndRed(gameBoard, LEFT_REEF_CUP_GREEN_RIGHT_X, LEFT_REEF_CUP_GREEN_RIGHT_Y);
+
+    // Central Cups
+    cupRedAndGreen(gameBoard, CENTRAL_CUP_RED_LEFT_X, CENTRAL_CUP_RED_LEFT_Y);
+    cupGreenAndRed(gameBoard, CENTRAL_CUP_GREEN_RIGHT_X, CENTRAL_CUP_GREEN_RIGHT_Y);
+
+    // Drop Area Cups
+    cupGreenAndRed(gameBoard, DROP_AREA_CUP_GREEN_LEFT_X, DROP_AREA_CUP_GREEN_LEFT_Y);
+    cupRedAndGreen(gameBoard, DROP_AREA_CUP_RED_RIGHT_X, DROP_AREA_CUP_RED_RIGHT_Y);
+    cupRedAndGreen(gameBoard, DROP_AREA_CUP_RED_LEFT_X, DROP_AREA_CUP_RED_LEFT_Y);
+    cupGreenAndRed(gameBoard, DROP_AREA_CUP_GREEN_RIGHT_X, DROP_AREA_CUP_GREEN_RIGHT_Y);
+
+    setGameBoardCurrentColor(gameBoard, CONSOLE_COLOR_WHITE);
+}
+
 void addGameBoardElements2020(GameBoardElementList* gameBoardElementList) {
     // Excluding zone
     addGameBoardElement(gameBoardElementList, &windowExclusion2020Print, &windowExclusionNotReachable2020, GAME_BOARD_ELEMENT_REACHABLE_ONLY);
 
     // Fake Robot
     addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &fakeRobotPrint2020);
+
+    // Cups
+    addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &cupPrints);
+
+    // Start Areas : TODO : Change to unreachable
+    addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &startAreaPrint);
+
+    // Protection Areas : TODO : Change to unreachable
+    addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &protectionDropAreasPrint);
+
+    // Drop Areas
+    addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &dropAreasPrint);
+
+    // Right Reef
+    addReachableByOpponentRobotGameBoardElement(gameBoardElementList, &rightReefPrint);
 }
--- robot/2020/gameboard/gameBoardElement2020.h
+++ robot/2020/gameboard/gameBoardElement2020.h
@@ -3,6 +3,92 @@
 
 #include "../../../robot/gameboard/gameboardElementList.h"
 
+#define CUP_RADIUS                                                    3.5f
+
+// Near Start Area
+
+#define START_AREA_CUP_RED_LEFT_X                                      400
+#define START_AREA_CUP_RED_LEFT_Y                                      300
+
+#define START_AREA_CUP_GREEN_LEFT_X                                    510
+#define START_AREA_CUP_GREEN_LEFT_Y                                    450
+
+#define START_AREA_CUP_GREEN_RIGHT_X                                  1080
+#define START_AREA_CUP_GREEN_RIGHT_Y           START_AREA_CUP_GREEN_LEFT_Y
+
+#define START_AREA_CUP_RED_RIGHT_X                                    1200
+#define START_AREA_CUP_RED_RIGHT_Y               START_AREA_CUP_RED_LEFT_Y
+
+// Near Reef
+
+#define LEFT_REEF_CUP_RED_LEFT_X                                       100
+#define LEFT_REEF_CUP_RED_LEFT_Y                                       670
+
+#define LEFT_REEF_CUP_GREEN_RIGHT_X                                    400
+#define LEFT_REEF_CUP_GREEN_RIGHT_Y                                    950
+
+// Central
+
+#define CENTRAL_CUP_RED_LEFT_X                                         800
+#define CENTRAL_CUP_RED_LEFT_Y                                        1100
+
+#define CENTRAL_CUP_GREEN_RIGHT_X                                     1270
+#define CENTRAL_CUP_GREEN_RIGHT_Y                                     1200
+
+// Drop Area
+
+// -> Cup Bottom
+#define DROP_AREA_CUP_GREEN_LEFT_X                                    1650
+#define DROP_AREA_CUP_GREEN_LEFT_Y                                    1665
+
+#define DROP_AREA_CUP_RED_RIGHT_X                                     1955
+#define DROP_AREA_CUP_RED_RIGHT_Y                                     1605
+
+// -> Cup Up
+#define DROP_AREA_CUP_RED_LEFT_X                DROP_AREA_CUP_GREEN_LEFT_X
+#define DROP_AREA_CUP_RED_LEFT_Y                                      1935
+
+#define DROP_AREA_CUP_GREEN_RIGHT_X              DROP_AREA_CUP_RED_RIGHT_X
+#define DROP_AREA_CUP_GREEN_RIGHT_Y                                   1995
+
+// -> Drop Protection
+#define DROP_AREA_RECTANGLE_PROTECTION_LEFT_X                         1850
+#define DROP_AREA_RECTANGLE_PROTECTION_LEFT_Y                          889
+#define DROP_AREA_RECTANGLE_PROTECTION_LEFT_WIDTH                      150
+#define DROP_AREA_RECTANGLE_PROTECTION_LEFT_HEIGHT                      22
+
+// -> Drop Area Rectangles
+#define DROP_AREA_RECTANGLE_GREEN_LEFT_X                              1700
+#define DROP_AREA_RECTANGLE_GREEN_LEFT_Y                              1650
+#define DROP_AREA_RECTANGLE_GREEN_WIDTH                                300
+#define DROP_AREA_RECTANGLE_GREEN_HEIGHT                               100
+
+#define DROP_AREA_RECTANGLE_RED_LEFT_X    DROP_AREA_RECTANGLE_GREEN_LEFT_X
+#define DROP_AREA_RECTANGLE_RED_LEFT_Y                                1850
+#define DROP_AREA_RECTANGLE_RED_WIDTH      DROP_AREA_RECTANGLE_GREEN_WIDTH
+#define DROP_AREA_RECTANGLE_RED_HEIGHT     DROP_AREA_RECTANGLE_GREEN_HEIGHT
+
+// -> Right Reef
+#define RIGHT_REEF_RECTANGLE_LEFT_X                                   1390
+#define RIGHT_REEF_RECTANGLE_LEFT_Y                                      0
+#define RIGHT_REEF_RECTANGLE_WIDTH                                     419
+#define RIGHT_REEF_RECTANGLE_HEIGHT                                     10
+
+
+// -> Start Area Rectangles
+#define START_AREA_RECTANGLE_WITH                                      600
+
+#define START_AREA_RECTANGLE_GREEN_LEFT_X                              500
+#define START_AREA_RECTANGLE_GREEN_LEFT_Y                                0
+#define START_AREA_RECTANGLE_GREEN_WIDTH                                30
+#define START_AREA_RECTANGLE_GREEN_HEIGHT                              400
+
+#define START_AREA_RECTANGLE_RED_LEFT_X                               1070
+#define START_AREA_RECTANGLE_RED_LEFT_Y  START_AREA_RECTANGLE_GREEN_LEFT_Y
+#define START_AREA_RECTANGLE_RED_WIDTH    START_AREA_RECTANGLE_GREEN_WIDTH
+#define START_AREA_RECTANGLE_RED_HEIGHT  START_AREA_RECTANGLE_GREEN_HEIGHT
+
+
 /**
  * Add the 2020 Gameboard elementzs.
  */
